68a1bb921dfd875e80992214b87c3d3b9b310c34
Paul Aguirre <paulagu.51763@redlandsschools.net>
2017-12-05 17:40:55 +0000

    hi

---
+++ Aguirre.py
@@ -0,0 +1,19 @@
+x=int(input("please enter a number and I will mutilpy it by 2: "))
+rint(x*2)
+
+
+
+um = int(input("Enter a number and I will dtermine if it is odd or even: "))
+mod = num % 2
+if mod > 0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
+
+
+import turtle
+sven=turtle.Turtle()
+y-int(input("enter a number and i will make rectangle"))
+for i in range(4):
+    sven.forward(y+100)
+    sven.right(y+90)
